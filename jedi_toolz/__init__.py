5c2ae64eeb0080767cbe2375de1fc16e612ee230
JediHero <hansen.rusty@gmail.com>
2020-04-28 16:01:19 -0600

    v 0.0.5

--- jedi_toolz/__init__.py
+++ jedi_toolz/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from jedi_toolz import config
 from jedi_toolz.show import show
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 import jedi_toolz as jt
 
 def test_init():
-    assert jt.__version__ == "0.0.4"
+    assert jt.__version__ == "0.0.5"
